3c23213621adb821b1e2c59f6c22a44ff2677f4f
Jon López Garrido <jonlopezgarrido@gmail.com>
2024-02-24 19:36:20 +0000

    Instalación y configuración del linter y el formater, usando eslint y prettier

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,7 +1,11 @@
-import ReactDOM from "react-dom";
-import "./index.css"
+import React from 'react';
+
+import ReactDOMClient from 'react-dom/client';
+import './index.css';
 
 const app = <h1>hola compadres</h1>;
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 
-ReactDOM.render(app, container);
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<React.StrictMode>{app}</React.StrictMode>);
